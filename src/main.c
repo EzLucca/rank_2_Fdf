950dabe57347f18691bb4e6d08d348876fb272b7
Eduardo Lucca <edlucca@student.hive.fi>
2025-07-04 17:09:18 +0300

    start parsing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: edlucca <edlucca@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 14:46:45 by edlucca           #+#    #+#             */
-/*   Updated: 2025/07/04 15:29:04 by edlucca          ###   ########.fr       */
+/*   Updated: 2025/07/04 17:09:00 by edlucca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,24 @@ static void ft_error(char *str)
 void parse_map(int file, int x, int y)
 {
 	t_parse	m;
+	char	*str;
 
 	ft_bzero(&m, sizeof(t_parse));
 	m.file = file;
+	str = get_next_line(file);
+	while(*str)
+	{
+		// split
+		// count elementes in string
+		//
+	}
+
+	// get next line to read first line
+	//
+	// needs to get the x value to compare with the others
+	// or count the number of ints and divide until remainder is zero
+	//
+	// split
 
 }
 
@@ -41,7 +56,7 @@ int	main(int argc, char **argv)
 	file = open(argv[1], O_RDONLY);
 	if (file == -1)
 		ft_error("Can't open file.\n");
-	// Parsing call
+	// Parsing 
 	fdf.points = parse_map(file, fdf.points->map_x, fdf.points->map_z);
 	close(file);
 	// draw lines
